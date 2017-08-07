265bc323824aba7c3578f3d30bd1e10680695a2c
YeonJung Kim <duswnd25@gmail.com>
2017-08-07 21:47:43 +0900

    컨트롤러 분리

---
+++ public/js/blog_controller.js
@@ -0,0 +1,27 @@
+let app = angular.module("trendReaderApp", []);
+
+app.controller("lezhin_controller", function ($scope, $http) {
+    $http.get("./api/blog/lezhin").then(function (result) {
+        console.log(result);
+        $scope.blog_name = result.data.blog_name;
+        $scope.blog_favicon_src = result.data.blog_favicon_src;
+        $scope.blog_header_image_src = result.data.blog_header_src;
+        $scope.article_title = result.data.article_title;
+        $scope.article_link = result.data.article_link;
+        $scope.article_summary = result.data.article_summary;
+        $scope.blog_update = result.data.article_date;
+    });
+});
+
+app.controller("drama_controller", function ($scope, $http) {
+    $http.get("./api/blog/drama").then(function (result) {
+        console.log(result);
+        $scope.blog_name = result.data.blog_name;
+        $scope.blog_favicon_src = result.data.blog_favicon_src;
+        $scope.blog_header_image_src = result.data.blog_header_src;
+        $scope.article_title = result.data.article_title;
+        $scope.article_link = result.data.article_link;
+        $scope.article_summary = result.data.article_summary;
+        $scope.blog_update = result.data.article_date;
+    });
+});
