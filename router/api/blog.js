2f3f8a7a55daec22a18d725a225629cc7c758719
YeonJung Kim <duswnd25@gmail.com>
2017-08-07 21:51:11 +0900

    로그삭제

--- router/api/blog.js
+++ router/api/blog.js
@@ -4,7 +4,6 @@ let parseManager = require(rootPath + '/data/parser/parse_manager');
 module.exports = function (app) {
     app.get('/api/blog/:type', (req, res) => {
         parseManager.getData(req.params.type, function (result) {
-            console.log(result);
             if (result.blog_name === 'NODATA') {
                 return res.status(404).json({error: '종류를 입력해주세요.'});
             } else {
