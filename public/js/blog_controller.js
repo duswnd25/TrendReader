6a53aafd6c073738821ca13166d6120f0f79d251
YeonJung Kim <duswnd25@gmail.com>
2017-08-07 22:49:50 +0900

    함수로 분리

--- public/js/blog_controller.js
+++ public/js/blog_controller.js
@@ -1,7 +1,7 @@
 let app = angular.module("trendReaderApp", []);
 
-app.controller("lezhin_controller", function ($scope, $http) {
-    $http.get("./api/blog/lezhin").then(function (result) {
+function initData($scope, $http, type) {
+    $http.get("./api/blog/" + type).then(function (result) {
         console.log(result);
         $scope.blog_name = result.data.blog_name;
         $scope.blog_favicon_src = result.data.blog_favicon_src;
@@ -11,30 +11,20 @@ app.controller("lezhin_controller", function ($scope, $http) {
         $scope.article_summary = result.data.article_summary;
         $scope.blog_update = result.data.article_date;
     });
+}
+
+app.controller("lezhin_controller", function ($scope, $http) {
+    initData($scope, $http, 'lezhin');
 });
 
 app.controller("drama_controller", function ($scope, $http) {
-    $http.get("./api/blog/drama").then(function (result) {
-        console.log(result);
-        $scope.blog_name = result.data.blog_name;
-        $scope.blog_favicon_src = result.data.blog_favicon_src;
-        $scope.blog_header_image_src = result.data.blog_header_src;
-        $scope.article_title = result.data.article_title;
-        $scope.article_link = result.data.article_link;
-        $scope.article_summary = result.data.article_summary;
-        $scope.blog_update = result.data.article_date;
-    });
+    initData($scope, $http, 'drama');
 });
 
 app.controller("rainist_controller", function ($scope, $http) {
-    $http.get("./api/blog/rainist").then(function (result) {
-        console.log(result);
-        $scope.blog_name = result.data.blog_name;
-        $scope.blog_favicon_src = result.data.blog_favicon_src;
-        $scope.blog_header_image_src = result.data.blog_header_src;
-        $scope.article_title = result.data.article_title;
-        $scope.article_link = result.data.article_link;
-        $scope.article_summary = result.data.article_summary;
-        $scope.blog_update = result.data.article_date;
-    });
+    initData($scope, $http, 'rainist');
+});
+
+app.controller("spoqa_controller", function ($scope, $http) {
+    initData($scope, $http, 'spoqa');
 });
