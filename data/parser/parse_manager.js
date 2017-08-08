b7f333fabd9ac49f636038c027b9391bda36603d
YeonJung Kim <duswnd25@gmail.com>
2017-08-08 16:42:57 +0900

    간략화

--- data/parser/parse_manager.js
+++ data/parser/parse_manager.js
@@ -1,46 +1,13 @@
-const lezhin = require('./blog/lezhin');
-const drama = require('./blog/drama');
-const rainist = require('./blog/rainist');
-const spoqa = require('./blog/spoqa');
-const jandi = require('./blog/jandi');
-const sk = require('./blog/sk');
-
 exports.getData = function (type, rootCallback) {
-    switch (type) {
-        case 'lezhin':
-            lezhin.getData(function (result) {
-                rootCallback(result);
-            });
-            break;
-        case 'drama':
-            drama.getData(function (result) {
-                rootCallback(result);
-            });
-            break;
-        case 'rainist':
-            rainist.getData(function (result) {
-                rootCallback(result);
-            });
-            break;
-        case 'spoqa':
-            spoqa.getData(function (result) {
-                rootCallback(result);
-            });
-            break;
-        case 'jandi':
-            jandi.getData(function (result) {
-                rootCallback(result);
-            });
-            break;
-        case 'sk':
-            sk.getData(function (result) {
-                rootCallback(result);
-            });
-            break;
-        default:
-            const resultItem = require('./result_item');
-            let data = resultItem.getResultItem();
-            data.blog_name = 'NODATA';
-            rootCallback(data);
+    try {
+        const parser = require('./blog/' + type);
+        parser.getData(function (result) {
+            rootCallback(result);
+        });
+    } catch (exception) {
+        const resultItem = require('./result_item');
+        let data = resultItem.getResultItem();
+        data.blog_name = 'NODATA';
+        rootCallback(data);
     }
 }
