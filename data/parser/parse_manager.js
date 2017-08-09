eed05cbde05426db8635719a6c9bdf11a131e3ee
YeonJung Kim <duswnd25@gmail.com>
2017-08-09 19:57:32 +0900

    전체 api

--- data/parser/parse_manager.js
+++ data/parser/parse_manager.js
@@ -1,13 +1,17 @@
 exports.getData = function (type, rootCallback) {
-    try {
-        const parser = require('./blog/' + type);
-        parser.getData(function (result) {
-            rootCallback(result);
-        });
-    } catch (exception) {
-        const resultItem = require('./result_item');
-        let data = resultItem.getResultItem();
-        data.blog_name = 'NODATA';
-        rootCallback(data);
+    if (type === 'all') {
+
+    } else {
+        try {
+            const parser = require('./blog/' + type);
+            parser.getData(function (result) {
+                rootCallback(result);
+            });
+        } catch (exception) {
+            const resultItem = require('./result_item');
+            let data = resultItem.getResultItem();
+            data.blog_name = 'NODATA';
+            rootCallback(data);
+        }
     }
 }
