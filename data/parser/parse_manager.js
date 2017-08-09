91a149510e9f7aaf5966e36d5585355a6e85293c
YeonJung Kim <duswnd25@gmail.com>
2017-08-10 01:54:09 +0900

    fs 추가

--- data/parser/parse_manager.js
+++ data/parser/parse_manager.js
@@ -1,6 +1,18 @@
 exports.getData = function (type, rootCallback) {
     if (type === 'all') {
+        const fs = require('fs');
+        let path = "/folderPath";
 
+        fs.readdir(path, function (err, folders) {
+
+            for (let i = 0; i < folders.length; i++) {
+                let folder = folders[i];
+                let fPath = path + "/" + folder;  // 하위 폴더 경로 반환
+                let files = fs.readdirSync(fPath);  // 하위 폴더 내 파일 검색
+                console.log(fPath);
+                console.log(files);
+            }
+        });
     } else {
         try {
             const parser = require('./blog/' + type);
