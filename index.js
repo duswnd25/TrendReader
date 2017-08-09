ae000d89b5ae7761d27edcf40045145a94fb8555
YeonJung Kim <duswnd25@gmail.com>
2017-08-09 21:58:01 +0900

    주석

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 require ('newrelic');
+
+// Module
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 1234;
 
-
+// Router
 const mainRouter = require('./router/main/main.js')(app);
 const apiRouter = require('./router/api/blog.js')(app);
 
+// Express 설정
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
