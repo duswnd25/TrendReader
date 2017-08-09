ec5bd780906f162d6668fd638dfd533f01718483
YeonJung Kim <duswnd25@gmail.com>
2017-08-10 04:40:05 +0900

    JSON Array 개선
    현재 상태로는 제일 마지막 값 만이 들어간다. JSON에 모든 데이터를 넣도록 개선해보자

--- data/parser/parse_manager.js
+++ data/parser/parse_manager.js
@@ -1,25 +1,30 @@
 exports.getData = function (type, rootCallback) {
     if (type === 'all') {
-        const fs = require('fs');
-        let path = "/folderPath";
+        let fs = require('fs');
+        let path = process.cwd() + '/data/parser/blog';
 
-        fs.readdir(path, function (err, items) {
-            console.log(items);
-            for (let index = 0; index < items.length; index++) {
-                console.log(items[index]);
-            }
+        let blogList = fs.readdirSync(path);
+        let result = JSON.parse('[]');
+        let counter = 1;
+
+        blogList.forEach(file => {
+            let parser = require('./blog/' + file);
+            parser.getData(function (data) {
+                result.push(data);
+                counter === blogList.length ? rootCallback(data) : counter++;
+            });
         });
     } else {
         try {
-            const parser = require('./blog/' + type);
+            let parser = require('./blog/' + type);
             parser.getData(function (result) {
                 rootCallback(result);
             });
         } catch (exception) {
-            const resultItem = require('./result_item');
+            let resultItem = require('./result_item');
             let data = resultItem.getResultItem();
             data.blog_name = 'NODATA';
             rootCallback(data);
         }
     }
-}
+};
