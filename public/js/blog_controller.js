79663c757e91d5188e626aa0f34c890ee622c22f
YeonJung Kim <duswnd25@gmail.com>
2017-08-09 19:57:51 +0900

    뷰를 만들어야하는데 어떻게 연결해야할지 모르겠다

--- public/js/blog_controller.js
+++ public/js/blog_controller.js
@@ -13,50 +13,44 @@ let app = angular.module("trendReaderApp", []);
 function view_create($http, item, callback) {
     $http.get("./api/blog/" + item).then(function (result) {
 
-        let view = '<!-- Blog Start -->\n' +
-            '    <div class="grid-item">\n' +
-            '        <a href="' + result.data.article_link + '">\n' +
-            '            <!-- CardView Start -->\n' +
-            '            <md-card class="_md">\n' +
-            '                <md-card-header>\n' +
-            '                    <md-card-avatar>\n' +
-            '                        <!-- Logo Image-->\n' +
-            '                        <img src="' + result.data.blog_favicon_src + '">\n' +
-            '                    </md-card-avatar>\n' +
-            '                    <md-card-header-text>\n' +
-            '                        <span class="md-title">' + result.data.blog_name + '</span>\n' +
-            '                        <span class="md-subhead">' + result.data.article_date + '</span>\n' +
-            '                    </md-card-header-text>\n' +
-            '                </md-card-header>\n' +
-            '                <img src="' + result.data.blog_header_src + '" class="md-card-image" style="height: 100px">\n' +
-            '                <md-card-title>\n' +
-            '                    <md-card-title-text>\n' +
-            '                        <span class="md-headline">' + result.data.article_title + '</span>\n' +
-            '                    </md-card-title-text>\n' +
-            '                </md-card-title>\n' +
-            '                <md-card-content>\n' +
-            '                    <p>\n' +
-            '                        ' + result.data.article_summary + '\n' +
-            '                    </p>\n' +
-            '                </md-card-content>\n' +
-            '            </md-card>\n' +
-            '            <!-- CardView End -->\n' +
-            '        </a>\n' +
-            '    </div>\n' +
-            '    <!-- Blog End -->';
+        let view =
+            '<div class="col-md-3 tr-container">\n' +
+            '    <a href="\' + result.data.  + \'" class="card">\n' +
+            '        <div class="tr-blog-info-container">\n' +
+            '            <img class="tr-favicon" src="\' + result.data.blog_favicon_src + \'"\n' +
+            '                 alt="블로그 favicon">\n' +
+            '            <h3 class="tr-blog-name">\' + result.data.blog_name + \'</h3>\n' +
+            '        </div>\n' +
+            '        <div class="text-center">\n' +
+            '            <img class="card-img-top tr-article-image img-responsive" alt="게시글 이미지" +\n' +
+            '                 src="\' + result.data.blog_header_src + \'" data-holder-rendered="true">\n' +
+            '            <div class="card-block">\n' +
+            '                <h4 class="card-title blog-article-title text-center">\' + result.data.article_title + \'</h4>\n' +
+            '                <p class="card-text blog-article-summary">\' + result.data.article_summary +\'\n' +
+            '                </p>\n' +
+            '            </div>\n' +
+            '        </div>\n' +
+            '    </a>\n' +
+            '</div>;'
 
         callback(view);
     });
 }
 
-app.controller("blog_controller", function ($scope, $http) {
-    let list = [
-        'drama', 'lezhin', 'spoqa', 'rainist', 'jandi', 'sk', 'add2paper', 'realm', 'linchpin', 'outsider'
-    ];
-
-    list.forEach(function (item) {
-        view_create($http, item, function (result) {
-            $('#blog_view').append(result);
-        });
+function getData($http, item, callback) {
+    $http.get("./api/blog/" + item).then(function (result) {
+        callback(result);
     });
+}
+
+app.controller("blog_controller", function ($scope, $http) {
+    $scope.list = [
+        getData($http, 'lezhin', function (result) {
+
+        })
+    ];
+    $http.get("./api/blog/" + $scope.list.name)
+        .then(function (result) {
+            $scope.list.data = result;
+        });
 });
--- router/api/blog.js
+++ router/api/blog.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
     app.get('/api/blog/:type', (req, res) => {
         parseManager.getData(req.params.type, function (result) {
             if (result.blog_name === 'NODATA') {
-                return res.status(404).json({error: '종류를 입력해주세요.'});
+                return res.status(404).json({error: '종류를 입력해주세요.', value: req.params.type});
             } else {
                 return res.json(result);
             }
