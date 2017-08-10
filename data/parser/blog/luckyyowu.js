169ff9f2e641222b672e4dc6ebb54d34d73892cd
YeonJung Kim <duswnd25@gmail.com>
2017-08-11 04:29:00 +0900

    블로그 추가

---
+++ data/parser/blog/luckyyowu.js
@@ -0,0 +1,46 @@
+// TODO 변경필요
+const blogName = '요우의 내맘대로 블로그';
+const rootUrl = 'http://luckyyowu.tistory.com/';
+const headerSrc = 'http://cfile4.uf.tistory.com/image/25438D4353F39A252F6A0B'; // 표시 없으
+// Module
+const cheerio = require('cheerio');
+const requestPromise = require('request-promise');
+
+const rootPath = process.cwd();
+const resultItem = require(rootPath + '/data/parser/result_item');
+
+exports.getData = function (rootCallback) {
+    requestPromise(rootUrl)
+        .then(function (htmlString) {
+            let $ = cheerio.load(htmlString);
+
+            let articleItem = $('div.list_content').eq(0);
+
+            // Title
+            let titleItem = articleItem.children('a.link_post').eq(0);
+            let parseTitle = titleItem.children('strong.tit_post').eq(0).text();
+            let parseLink = titleItem.attr('href');
+
+            // Date
+            let parseDate = articleItem.children('div.detail_info').eq(0).text(); // 표시 없음
+
+            // Summary
+            let parseSummary = titleItem.children('p.txt_post').eq(0).text(); // 표시 없음
+
+            // Result
+            let result = resultItem.getResultItem();
+            result.blog_name = blogName;
+            result.blog_favicon_src = 'https://www.google.com/s2/favicons?domain=' + rootUrl;
+            result.blog_header_src = headerSrc;
+            result.article_title = parseTitle;
+            result.article_date = parseDate;
+            result.article_link = rootUrl + parseLink;
+            result.article_summary = parseSummary.length > 200 ? parseSummary.substring(0, 200) : parseSummary;
+            result.blog_type = 'P';
+
+            rootCallback(result);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+};
