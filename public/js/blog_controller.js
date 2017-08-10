82125db7eab62779d928d3d0b6379c656273f8b9
YeonJung Kim <duswnd25@gmail.com>
2017-08-11 03:32:42 +0900

    최적화

--- public/js/blog_controller.js
+++ public/js/blog_controller.js
@@ -1,16 +1,1 @@
-// AngularJS
-let app = angular.module("trendReaderApp", []);
-
-app.controller("blog_controller", function ($scope, $http) {
-    $http.get("./api/blog/all")
-        .then(function (result) {
-            $scope.list = result.data;
-            // jQuery
-            let $container = jQuery('#masonry-grid');
-            // initialize
-            $container.masonry({
-                itemSelector: '#masonry-item',
-                isAnimated: true
-            });
-        });
-});
+let app=angular.module("trendReaderApp",[]);app.controller("blog_controller",function($scope,$http){$http.get("./api/blog/all").then(function(result){$scope.list=result.data;let $container=jQuery('#masonry-grid');$container.masonry({itemSelector:'#masonry-item',isAnimated:true});});});
