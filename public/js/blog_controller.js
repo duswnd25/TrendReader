70acf255fb670d45cb4bb017956146fca35f8e71
YeonJung Kim <duswnd25@gmail.com>
2017-08-11 16:01:47 +0900

    몽구스 추가

--- public/js/blog_controller.js
+++ public/js/blog_controller.js
@@ -1,6 +1,22 @@
 let app = angular.module("trendReaderApp", []);
 app.controller("blog_controller", function ($scope, $http) {
 
-    $scope.blogList = ["lezhin", "rainist", "luckyyowu"];
+    $scope.blogList = [
+        'add2paper',
+        'drama',
+        'jandi',
+        'kakao',
+        'lezhin',
+        'linchpin',
+        'line',
+        'luckyyowu',
+        'outsider',
+        'rainist',
+        'realm',
+        'sk',
+        'spoqa',
+        'unity',
+        'woowabros'
+    ];
 
 });
