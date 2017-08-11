25b3948685902c0cf34164ffa109ff0de442964a
YeonJung Kim <duswnd25@gmail.com>
2017-08-12 01:38:15 +0900

    역할 변경
    이 파일은 기존에 요청이 들어오면 요청에 맞게 파서를 불러서 결과를 반환하는 역할에서 일정 주기마다 전체 파서를 돌리는 역할로 변경함

--- data/parser/parse_manager.js
+++ data/parser/parse_manager.js
@@ -1,38 +1,22 @@
-exports.getData = function (type, rootCallback) {
-    if (type === 'all') {
-        let fs = require('fs');
-        let path = process.cwd() + '/data/parser/blog';
+let fs = require('fs');
+let path = process.cwd() + '/data/parser/blog';
 
-        let blogList = fs.readdirSync(path);
-        let result = '[';
-        let counter = 1;
+let blogList = fs.readdirSync(path);
+let result = '[';
+let counter = 1;
 
-        blogList.forEach(file => {
-                let parser = require('./blog/' + file);
-                parser.getData(function (data) {
-                    data = JSON.stringify(data);
-                    result += result === '[' ? data : ',' + data;
-                    if (counter === blogList.length) {
-                        result += ']';
-                        rootCallback(JSON.parse(result))
-                    }
-                    else {
-                        counter++;
-                    }
-                });
+blogList.forEach(file => {
+        let parser = require('./blog/' + file);
+        parser.getData(function (data) {
+            data = JSON.stringify(data);
+            result += result === '[' ? data : ',' + data;
+            if (counter === blogList.length) {
+                result += ']';
+                rootCallback(JSON.parse(result))
             }
-        );
-    } else {
-        try {
-            let parser = require('./blog/' + type);
-            parser.getData(function (result) {
-                rootCallback(result);
-            });
-        } catch (exception) {
-            let resultItem = require('./result_item');
-            let data = resultItem.getResultItem();
-            data.blog_name = 'NODATA';
-            rootCallback(data);
-        }
+            else {
+                counter++;
+            }
+        });
     }
-};
+);
