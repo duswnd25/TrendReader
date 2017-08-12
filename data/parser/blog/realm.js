4ec030ddc2c456cb0806f9fc225e5748412b3e31
YeonJung Kim <duswnd25@gmail.com>
2017-08-12 21:37:10 +0900

    파싱 오류 수정

--- data/parser/blog/realm.js
+++ data/parser/blog/realm.js
@@ -1,5 +1,6 @@
 // TODO 변경필요
 const rootUrl = 'https://blog.realm.io/kr/';
+let headerSrc = 'https://realm.io/assets/svg/products/realm-mobile-platform/rmp-react-to-changes-in-real-time.svg';
 
 // Module
 const cheerio = require('cheerio');
@@ -19,27 +20,21 @@ exports.getData = function (rootCallback) {
             let articleItem = $('div.post.quick').eq(0).children('div.article-block.flex.center.column').eq(0);
 
             // Title
-            let titleItem = articleItem.children('a.news-headline.col-xs-12.col-sm-10.text-center').eq(0);
-            let parseTitle = titleItem.text();
-            let parseLink = titleItem.attr('href');
-
-            // Header
-            let parseHeaderSrc = 'https://realm.io/assets/svg/products/realm-mobile-platform/rmp-react-to-changes-in-real-time.svg';
-
-            // Date
-            let parseDate = ''; // 표시 없음
+            let titleItem = articleItem.children('a').eq(0);
+            let parseTitle = titleItem.children('h3').eq(0).text();
+            let parseLink = 'https://blog.realm.io' + titleItem.attr('href');
 
             // Summary
-            let parseSummary = ''; // 표시 없음
+            let parseSummary = articleItem.children('div.excerpt.col-xs-12.text-left').eq(0)
+                .children('div.summary').eq(0).text(); // 표시 없음
 
             // Result
             let result = resultItem.getResultItem();
             result.blog_name = blogName;
             result.blog_favicon_src = 'https://www.google.com/s2/favicons?domain=' + rootUrl;
-            result.blog_header_src = parseHeaderSrc;
+            result.blog_header_src = headerSrc;
             result.article_title = parseTitle;
-            result.article_date = parseDate;
-            result.article_link = 'https://news.realm.io' + parseLink;
+            result.article_link = parseLink;
             result.article_summary = parseSummary.length > 200 ? parseSummary.substring(0, 200) : parseSummary;
             result.blog_type = 'C';
 
