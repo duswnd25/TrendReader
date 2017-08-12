67ae03662d74789027e3c6b69cc8ee3e4689aa6c
YeonJung Kim <duswnd25@gmail.com>
2017-08-12 11:25:40 +0900

    분기

--- data/parser/parse_manager.js
+++ data/parser/parse_manager.js
@@ -2,18 +2,30 @@ const fs = require('fs');
 const dbManager = require(process.cwd() + '/data/db/db_manager');
 const path = process.cwd() + '/data/parser/blog';
 
-exports.parseData = function () {
-    let blogList = fs.readdirSync(path);
-    blogList.forEach(file => {
-            let parser = require('./blog/' + file);
-            let tagName = file.replace('.js', '').toLowerCase();
-            parser.getData(function (data) {
-                dbManager.isNewData(tagName, data.article_title, function (isNewData) {
-                    if (isNewData) {
-                        dbManager.saveNewData(tagName, data);
-                    }
-                })
-            });
-        }
-    );
+exports.parseData = function (type) {
+    if (type === 'all') {
+        let blogList = fs.readdirSync(path);
+        blogList.forEach(file => {
+                let parser = require('./blog/' + file);
+                let tagName = file.replace('.js', '').toLowerCase();
+                parser.getData(function (data) {
+                    dbManager.isNewData(tagName, data.article_title, function (isNewData) {
+                        if (isNewData) {
+                            dbManager.saveNewData(tagName, data);
+                        }
+                    })
+                });
+            }
+        );
+    } else {
+        let parser = require('./blog/' + type);
+        console.log('s', type);
+        parser.getData(function (data) {
+            dbManager.isNewData(type, data.article_title, function (isNewData) {
+                if (isNewData) {
+                    dbManager.saveNewData(type, data);
+                }
+            })
+        });
+    }
 };
