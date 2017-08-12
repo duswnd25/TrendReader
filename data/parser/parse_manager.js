c11ef0708bb76c2756998533645d1b357b1b0625
YeonJung Kim <duswnd25@gmail.com>
2017-08-12 21:24:50 +0900

    중복 코드 정리

--- data/parser/parse_manager.js
+++ data/parser/parse_manager.js
@@ -6,25 +6,21 @@ exports.parseData = function (type) {
     if (type === 'all') {
         let blogList = fs.readdirSync(path);
         blogList.forEach(file => {
-                let parser = require('./blog/' + file);
-                let tagName = file.replace('.js', '').toLowerCase();
-                parser.getData(function (data) {
-                    dbManager.isNewData(tagName, data.article_title, function (isNewData) {
-                        if (isNewData) {
-                            dbManager.saveNewData(tagName, data);
-                        }
-                    })
-                });
+                parseData(file.replace('.js', '').toLowerCase())
             }
         );
     } else {
-        let parser = require('./blog/' + type);
-        parser.getData(function (data) {
-            dbManager.isNewData(type, data.article_title, function (isNewData) {
-                if (isNewData) {
-                    dbManager.saveNewData(type, data);
-                }
-            })
-        });
+        parseData(type);
     }
 };
+
+function parseData(type) {
+    let parser = require('./blog/' + type);
+    parser.getData(function (data) {
+        dbManager.isNewData(type, data.article_title, function (isNewData) {
+            if (isNewData) {
+                dbManager.saveNewData(type, data);
+            }
+        })
+    });
+}
