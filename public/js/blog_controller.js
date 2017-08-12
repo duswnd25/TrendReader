5aec1d758147b3873abe4a970988fe76e478f74e
YeonJung Kim <duswnd25@gmail.com>
2017-08-12 21:53:30 +0900

    일단 표시

--- public/js/blog_controller.js
+++ public/js/blog_controller.js
@@ -1,22 +1,9 @@
+// AngularJS
 let app = angular.module("trendReaderApp", []);
+
 app.controller("blog_controller", function ($scope, $http) {
-
-    $scope.blogList = [
-        'add2paper',
-        'drama',
-        'jandi',
-        'kakao',
-        'lezhin',
-        'linchpin',
-        'line',
-        'luckyyowu',
-        'outsider',
-        'rainist',
-        'realm',
-        'sk',
-        'spoqa',
-        'unity',
-        'woowabros'
-    ];
-
+    $http.get("./api/data/read/all")
+        .then(function (result) {
+            $scope.list = result.data;
+        });
 });
