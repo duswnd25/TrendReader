8e2a214f0a78fc588264137b5b4c95964873d321
YeonJung Kim <duswnd25@gmail.com>
2017-08-12 10:40:18 +0900

    주기적 파싱 추가

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const newrelic = require('newrelic');
+// New Relic
+const port = process.env.PORT || 1234;
 
 // Module
+const newrelic = require('newrelic');
 const bodyParser = require('body-parser');
+const schedule = require('node-schedule');
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 1234;
 const compression = require('compression');
+
+// Parse Manager
+const parseManager = require(process.cwd() + '/data/parser/parse_manager');
+
+// Express 설정
+const app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use("/public", express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 // gzip
 app.use(compression());
@@ -14,14 +27,11 @@ app.use(compression());
 const mainRouter = require('./router/main/main.js')(app);
 const apiRouter = require('./router/api/data.js')(app);
 
-// Express 설정
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-app.use("/public", express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
 const server = app.listen(port, function () {
-    console.log("Trend Reader Working on Port " + port)
+    console.log("Trend Reader Working on Port " + port);
+
+    // 매 30분 반복
+    let scheduler = schedule.scheduleJob('* 30 * * * *', function () {
+        parseManager.parseData();
+    });
 });
