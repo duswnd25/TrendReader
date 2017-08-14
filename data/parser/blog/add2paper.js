6fec9b32275b163c87428f65b4f7dea577bb6afe
YeonJung Kim <duswnd25@gmail.com>
2017-08-14 09:41:58 +0900

    링크파싱 오류 수정

--- data/parser/blog/add2paper.js
+++ data/parser/blog/add2paper.js
@@ -34,7 +34,7 @@ exports.getData = function (rootCallback) {
         result.blog_favicon_src = 'https://www.google.com/s2/favicons?domain=' + rootUrl;
         result.blog_header_src = headerSrc;
         result.article_title = parseTitle;
-        result.article_link = parseLink;
+        result.article_link = rootUrl + parseLink;
         result.article_summary = parseSummary.length > 200 ? parseSummary.substring(0, 200) : parseSummary;
         result.blog_type = 'C';
 
