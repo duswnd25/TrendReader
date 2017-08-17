4373ccab0c559729bc7442e72309fc937bbbc23d
YeonJung Kim <duswnd25@gmail.com>
2017-08-18 00:41:54 +0900

    잘못된 링크 수정

--- server/data/parser/blog/outsider.js
+++ server/data/parser/blog/outsider.js
@@ -36,7 +36,7 @@ exports.getData = function (rootCallback) {
         result.favicon_src = 'https://www.google.com/s2/favicons?domain=' + rootUrl;
         result.header_src = parseHeaderSrc;
         result.title = parseTitle;
-        result.link = parseLink;
+        result.link = rootUrl.replace('/category', '') + parseLink;
         result.summary = parseSummary.length > 200 ? parseSummary.substring(0, 200) : parseSummary;
         result.type = 'P';
 
