23bf57540e35b87988e76476b0638ca1a09acf42
YeonJung Kim <duswnd25@gmail.com>
2017-08-20 03:04:44 +0900

    tyle

---
+++ data/parser/blog/tyle.js
@@ -0,0 +1,48 @@
+// TODO 변경필요
+const rootUrl = 'https://blog.tyle.io/';
+const headerSrc = 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIj8+CjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiBoZWlnaHQ9IjMwMHB4IiB3aWR0aD0iMzAwcHgiIHZlcnNpb249IjEuMCIgdmlld0JveD0iLTMwMCAtMzAwIDYwMCA2MDAiIHhtbDpzcGFjZT0icHJlc2VydmUiPgo8Y2lyY2xlIHN0cm9rZT0iI0FBQSIgc3Ryb2tlLXdpZHRoPSIxMCIgcj0iMjgwIiBmaWxsPSIjRkZGIi8+Cjx0ZXh0IHN0eWxlPSJsZXR0ZXItc3BhY2luZzoxO3RleHQtYW5jaG9yOm1pZGRsZTt0ZXh0LWFsaWduOmNlbnRlcjtzdHJva2Utb3BhY2l0eTouNTtzdHJva2U6IzAwMDtzdHJva2Utd2lkdGg6MjtmaWxsOiM0NDQ7Zm9udC1zaXplOjM2MHB4O2ZvbnQtZmFtaWx5OkJpdHN0cmVhbSBWZXJhIFNhbnMsTGliZXJhdGlvbiBTYW5zLCBBcmlhbCwgc2Fucy1zZXJpZjtsaW5lLWhlaWdodDoxMjUlO3dyaXRpbmctbW9kZTpsci10YjsiIHRyYW5zZm9ybT0ic2NhbGUoLjIpIj4KPHRzcGFuIHk9Ii00MCIgeD0iOCI+Tk8gSU1BR0U8L3RzcGFuPgo8dHNwYW4geT0iNDAwIiB4PSI4Ij5BVkFJTEFCTEU8L3RzcGFuPgo8L3RleHQ+Cjwvc3ZnPg==';
+
+// Module
+const request = require('request');
+const cheerio = require('cheerio');
+
+const resultItem = require('../result_item');
+
+exports.getData = function (rootCallback) {
+    request(rootUrl, function (error, response, body) {
+
+        if (error) {
+            console.error(error);
+        }
+
+        let $ = cheerio.load(body);
+        
+        // Title
+        let blogName = $('title').eq(0).text().substring(0, 20);
+
+        // Article
+        let articleItem = $('div#posts-list').eq(0).children('a').eq(0);
+
+        console.log(articleItem.html());
+
+        // Title
+        let parseTitle = articleItem.children('span.post-subject').text();
+        let parseLink = articleItem.attr('href');
+        parseLink = rootUrl + parseLink;
+
+        // Summary
+        let parseSummary = articleItem.children('span.post-content').eq(0).text();
+
+        // Result
+        let result = resultItem.getResultItem();
+        result.name = blogName;
+        result.favicon_src = 'https://www.google.com/s2/favicons?domain=' + rootUrl;
+        result.header_src = headerSrc;
+        result.title = parseTitle;
+        result.link = parseLink;
+        result.summary = parseSummary.length > 200 ? parseSummary.substring(0, 200) : parseSummary;
+        result.type = 'C';
+
+        rootCallback(result);
+    });
+};
