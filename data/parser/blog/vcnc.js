ff3e7148301555b38d4cf93d539bc861c9df4cef
YeonJung Kim <duswnd25@gmail.com>
2017-09-04 00:25:11 +0900

    비트윈

---
+++ data/parser/blog/vcnc.js
@@ -0,0 +1,42 @@
+// TODO 변경필요
+const rootUrl = 'http://engineering.vcnc.co.kr';
+
+// Module
+const cheerio = require('cheerio');
+const request = require('request');
+
+const resultItem = require('../result_item');
+
+exports.getData = function (rootCallback) {
+    request(rootUrl, function (error, response, body) {
+
+        if (error) {
+            console.error(error);
+        }
+
+        let $ = cheerio.load(body);
+
+        // Name
+        let blogName = $('title').eq(0).text();
+
+        // Article
+        let articleItem = $('ul.archive').eq(0).children('li').eq(0).children('a').eq(0);
+
+        let parseTitle = articleItem.text();
+        let parseLink = articleItem.attr('href');
+        parseLink = rootUrl + parseLink;
+
+        // Summary
+        let parseSummary = '';
+
+        // Result
+        let result = resultItem.getResultItem();
+        result.name = blogName;
+        result.favicon_src = 'https://www.google.com/s2/favicons?domain=' + rootUrl;
+        result.title = parseTitle;
+        result.link = rootUrl + parseLink;
+        result.summary = parseSummary.length > 200 ? parseSummary.substring(0, 300) : parseSummary;
+
+        rootCallback(result);
+    });
+};
