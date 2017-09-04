68bb0a3dbff6f5c716acbdbf4a2ef800bc087566
YeonJung Kim <duswnd25@gmail.com>
2017-09-04 10:46:34 +0900

    이상한 모임

---
+++ data/parser/blog/weirdx.js
@@ -0,0 +1,39 @@
+// TODO 변경필요
+// D2는 Atom을 파싱한다.
+const rootUrl = 'http://blog.weirdx.io/feed';
+
+// Module
+const cheerio = require('cheerio');
+const request = require('request');
+
+const resultItem = require('../result_item');
+exports.getData = function (rootCallback) {
+    request(rootUrl, function (error, response, body) {
+
+        if (error) {
+            console.error(error);
+        }
+
+        let $ = cheerio.load(body, {
+            normalizeWhitespace: true,
+            xmlMode: true
+        });
+
+        // Title
+        let blogName = $('channel').eq(0).children('title').eq(0).text();
+
+        let articleItem = $('item').eq(0);
+        let parseTitle = articleItem.children('title').eq(0).text();
+        let parseLink = articleItem.children('link').eq(0).attr('href');
+        let parseSummary = articleItem.children('description').eq(0).text();
+
+        // Result
+        let result = resultItem.getResultItem();
+        result.name = blogName;
+        result.favicon_src = 'https://www.google.com/s2/favicons?domain=' + rootUrl;
+        result.title = parseTitle;
+        result.link = parseLink;
+        result.summary = parseSummary.length > 200 ? parseSummary.substring(0, 300) : parseSummary;
+        rootCallback(result);
+    });
+};
