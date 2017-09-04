0125c5806cb202873703a5079b098b46a9dc2a57
YeonJung Kim <duswnd25@gmail.com>
2017-09-04 11:09:50 +0900

    수정

--- router/main/main.js
+++ router/main/main.js
@@ -18,5 +18,5 @@ Date.prototype.getCustomType = function () {
     let hour = this.getHours().toString();
     let minute = this.getMinutes().toString();
     let second = this.getSeconds().toString();
-    return year + '년 ' + month + '월 ' + date + '일 ' + hour + '시 ' + minute + '분 ' + second + ' 초';
+    return year + '년 ' + month + '월 ' + date + '일 ' + hour + '시 ' + minute + '분 ' + second + '초';
 };
