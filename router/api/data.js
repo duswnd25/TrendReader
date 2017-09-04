635c6ee9cabf3bf03558f082bdcafcb046bb9c70
YeonJung Kim <duswnd25@gmail.com>
2017-09-04 15:23:01 +0900

    api 생성

--- router/api/data.js
+++ router/api/data.js
@@ -3,8 +3,9 @@ const parseManager = require('../../data/parser/parse_manager');
 const express = require('express');
 const router = express.Router();
 
-router.get('/read/:blogId', (req, res) => {
-    DbManager.getData(req.params.blogId, function (result) {
+// 읽기
+router.get('/read/blog/:blogId', (req, res) => {
+    DbManager.getDataByBlog(req.params.blogId, function (result) {
         if (result.name === '' || result.name === null) {
             return res.status(404).json({error: '종류를 입력해주세요.', value: req.params.blogId});
         } else {
@@ -13,19 +14,31 @@ router.get('/read/:blogId', (req, res) => {
     });
 });
 
+router.get('/read/type/:category', (req, res) => {
+    DbManager.getDataByCategory(req.params.category, function (result) {
+        if (result.name === '' || result.name === null) {
+            return res.status(404).json({error: '종류를 입력해주세요.', value: req.params.blogId});
+        } else {
+            return res.json(result);
+        }
+    });
+});
+
+// 파싱
 router.get('/parse/:blogId', (req, res) => {
     parseManager.parseData(req.params.blogId);
     return res.status(200).json({success: req.params.blogId + ' 파싱'});
 });
 
-router.get('/remove/:blogId', (req, res) => {
-    DbManager.removeData(req.params.blogId);
-    return res.status(200).json({success: req.params.blogId + ' 삭제'});
-});
-
 router.get('/parse/force_update/:blogId', (req, res) => {
     parseManager.forceUpdate(req.params.blogId);
     return res.status(200).json({success: req.params.blogId + ' 강제 업데이트'});
+});
+
+// 삭제
+router.get('/remove/:blogId', (req, res) => {
+    DbManager.removeData(req.params.blogId);
+    return res.status(200).json({success: req.params.blogId + ' 삭제'});
 });
 
 module.exports = router;
