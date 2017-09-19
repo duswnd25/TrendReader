2ab0c894be155348bf46584e0b4712fe799fe3eb
YeonJung Kim <duswnd25@gmail.com>
2017-09-19 17:37:09 +0900

    feedparser

---
+++ data/parser/feed_parser.js
@@ -0,0 +1,64 @@
+// 파서는 구글 Feed Burner에 등록한 주소를 서버에 저장 후
+// 별도의 스케줄러로 가져온다.
+const DBManager = require('./../db/database_manager');
+const FeedParser = require('feedparser');
+const Request = require('request');
+
+DBManager.getBlogList(function (results, error) {
+    if (error) {
+        console.error('PARSER : GET BLOG LIST ERROR = ' + error.code);
+        console.error(error.message);
+    } else {
+        console.log('PARSER : GET BLOG LIST SUCCESS');
+        results.forEach(function (item) {
+            parseFeed(item)
+        });
+    }
+});
+
+function parseFeed(item) {
+    let req = Request(item.feed_url);
+    let feedParser = new FeedParser({});
+
+    req.on('error', function (error) {
+        console.error('PARSER : REQUEST ERROR = ' + error.code);
+        console.error(error.message);
+    });
+
+    req.on('response', function (res) {
+        let stream = this;
+        if (res.statusCode !== 200) {
+            this.emit('error', new Error('Bad status code'));
+        }
+        else {
+            stream.pipe(feedParser);
+        }
+    });
+
+    feedParser.on('error', function (error) {
+        console.error('PARSER : FEED PARSER ERROR = ' + error.code);
+        console.error(error.message);
+    });
+
+    feedParser.once('readable', function () {
+        let stream = this;
+        let meta = this.meta;
+        let feed = stream.read();
+
+        let postContent = feed.description;
+        postContent.replace(/<br\/>/ig, "\n");
+        postContent = postContent.replace(/<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/ig, "");
+
+        let data = {
+            'blog_tag': item.blog_tag,
+            'blog_name': meta.title,
+            'post_title': feed.title,
+            'post_url': feed.link,
+            'post_content': postContent
+        };
+
+        DBManager.isNewData(feed.link, function (error, isNewData) {
+            if (isNewData && !error) DBManager.updateData(data);
+        });
+    });
+}
