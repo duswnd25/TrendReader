7ecdf007b6eecd8717206af19f0e2e1749b78086
YeonJung Kim <duswnd25@gmail.com>
2017-10-29 17:19:44 +0900

    code style

--- router/api/read.js
+++ router/api/read.js
@@ -1,12 +1,12 @@
 const DbManager = require("../../data/db/database_manager");
-const Express = require("express");
-const Router = Express.Router();
+const express = require("express");
+const Router = express.Router();
 
 // 읽기
 Router.get("/:target_column/:user_query", (req, res) => {
     DbManager.getData(req.params.target_column, req.params.user_query, function (result, error) {
         if (error) {
-            return res.status(404).json({error: '매개변수를 확인해 주세요.'});
+            return res.status(404).json({error: "매개변수를 확인해 주세요."});
         } else {
             return res.json(result);
         }
