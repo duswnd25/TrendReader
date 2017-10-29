3616f26d8bc2e35b2655dde209313fcbab506bf6
YeonJung Kim <duswnd25@gmail.com>
2017-10-29 22:05:02 +0900

    Revert "이름 변경"
    This reverts commit c1e823618c9f4ab041e06c9419c4da3f1ad7ea5c.

--- public/js/blog_controller.js
+++ public/js/blog_controller.js
@@ -1,5 +1,5 @@
 // AngularJS
-let app = angular.module("DevLogerApp", []);
+let app = angular.module("trendReaderApp", []);
 
 app.controller("blog_controller", function ($scope, $http) {
     $http.get("./api/data/read/blog/all")
