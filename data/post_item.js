735ce97fa7bb9d53880f5be32b553c84c02dcfbd
YeonJung Kim <duswnd25@gmail.com>
2017-10-29 16:57:48 +0900

    code style

--- data/post_item.js
+++ data/post_item.js
@@ -1,15 +1,15 @@
 exports.getResultItem = function () {
     return (
         {
-            'blog_tag': "",
-            'favicon_url': "",
-            'feed_url': "",
-            'blog_name': "",
-            'blog_url': "",
-            'post_title': "",
-            'post_url': "",
-            'post_content': "",
-            'update_at': ""
+            "blog_tag": "",
+            "favicon_url": "",
+            "feed_url": "",
+            "blog_name": "",
+            "blog_url": "",
+            "post_title": "",
+            "post_url": "",
+            "post_content": "",
+            "update_at": ""
         }
     );
 };
