b272fadd68c461b56e23a3e77ec513b3bfc4c3e9
YeonJung Kim <duswnd25@gmail.com>
2017-12-19 18:53:03 +0900

    레이아웃 테스트

--- public/js/blog_controller.js
+++ public/js/blog_controller.js
@@ -7,3 +7,12 @@ app.controller("blog_controller", function ($scope, $http) {
             $scope.list = result.data;
         });
 });
+
+$(window).load(function() {
+    $('.post-module').hover(function() {
+        $(this).find('.description').stop().animate({
+            height: "toggle",
+            opacity: "toggle"
+        }, 300);
+    });
+});
