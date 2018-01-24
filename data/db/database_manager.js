99266a1e34bdb1b9e2712972768830d04eb3336d
YeonJung Kim <duswnd25@gmail.com>
2018-01-25 00:28:04 +0900

    제한을 걸어보자

--- data/db/database_manager.js
+++ data/db/database_manager.js
@@ -52,6 +52,7 @@ exports.updateData = function (data) {
 exports.getData = function (target_column, user_query, callback) {
     let Post = Parse.Object.extend("Post");
     let query = new Parse.Query(Post);
+    query.limit(1000);
     query.descending("updatedAt");
     if (user_query !== "all") {
         query.equalTo(target_column, user_query);
