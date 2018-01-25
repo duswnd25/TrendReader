29fc221b4a210a47b893b7d45c3d25df247f3493
YeonJung Kim <duswnd25@gmail.com>
2018-01-25 16:25:42 +0900

    로그 추가

--- data/parser/feed_parser.js
+++ data/parser/feed_parser.js
@@ -10,7 +10,7 @@ DBManager.getData("blog", "all", function (result, error) {
         console.error(error.message);
     } else {
         console.log("DB : GET BLOG LIST SUCCESS");
-        
+        console.log(result);
         results.forEach(function (item) {
             parseFeed(item);
         });
