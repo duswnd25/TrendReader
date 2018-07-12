17d4794fe7dc8223d95d5bfc813da097c00b9ecb
duswnd25 <duswnd25@gmail.com>
2018-07-12 16:02:16 +0900

    수정

--- src/routes/main/main.js
+++ src/routes/main/main.js
@@ -1,12 +1,11 @@
 const dbManager = require("../../data/db/database_manager");
 const express = require("express");
 const router = express.Router();
-const createError = require('http-errors');
 
-router.get("/", function (req, res, next) {
+router.get("/", function (req, res) {
     dbManager.getData('blog', 'all', 20, function (result, error) {
         if (error) {
-            next(createError(404));
+            console.log(error);
         } else {
             res.render('index', {data: result});
         }
